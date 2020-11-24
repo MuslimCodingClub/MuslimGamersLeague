5ce3c8b6c354f7fff41877545e25ccb42ae5e58b
Muhammed Muratovic <muartovicm05@gmail.com>
2020-11-24 23:36:35 +0300

    Push 2.0

--- JS/main.js
+++ JS/main.js
@@ -26,3 +26,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "grid";
   dots[slideIndex-1].className += " active";
 }
+
+//Index More Games Button Code
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "View all games";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "View all games";
+    moreText.style.display = "inline";
+  }
+}
